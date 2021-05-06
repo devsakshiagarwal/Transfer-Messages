14dd7fd4737b62200e9491ba1cb4dd171427e3bf
Sakshi Agarwal <devsakshiagarwal@gmail.com>
2021-05-06 11:07:42 +0400

    added receiver logic for reading new message

--- app/src/main/java/com/goyals/transfernotification/model/broadcast_receiver/SmsBroadcastReceiver.kt
+++ app/src/main/java/com/goyals/transfernotification/model/broadcast_receiver/SmsBroadcastReceiver.kt
@@ -3,31 +3,61 @@ package com.goyals.transfernotification.model.broadcast_receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.telephony.SmsMessage
-import android.util.Log
+import android.database.Cursor
+import android.net.Uri
+import com.google.firebase.database.FirebaseDatabase
+import com.goyals.transfernotification.model.schema.Notification
+import com.goyals.transfernotification.utils.AppUtils
 
 class SmsBroadcastReceiver : BroadcastReceiver() {
   companion object {
-    private const val TAG = "SmsBroadcastReceive"
-    private const val pdu_type = "pdus"
+    private const val FIREBASE_REFERENCE = "notifications"
   }
 
-  override fun onReceive(context: Context,
-    intent: Intent) {
-    Log.d(TAG, "onReceive: ")
-    val bundle = intent.extras
-    val messages: Array<SmsMessage?>
-    var strMessage = ""
-    val format = bundle!!.getString("format")
-    val pduList = bundle[pdu_type] as Array<*>?
-    if (!pduList.isNullOrEmpty()) {
-      messages = arrayOfNulls(pduList.size)
-      for (i in messages.indices) {
-        messages[i] = SmsMessage.createFromPdu(pduList[i] as ByteArray, format)
-        strMessage += "SMS from  ${messages[i]?.originatingAddress}"
-        strMessage += " : ${messages[i]?.messageBody}"
-        Log.d(TAG, "onReceive: $strMessage")
+  /**
+   * called by default whenever a new message is received
+   */
+  override fun onReceive(context: Context?,
+    intent: Intent?) {
+    startSmsContentProvider(context)
+  }
+
+  /**
+   * read the message from content provider and add that to firebase realtime
+   * as @Notification object
+   */
+  private fun startSmsContentProvider(context: Context?) {
+    val notification: Notification =
+      AppUtils.getNotificationFromMessage(getFirstMessageFromProvider(context))
+    addNotificationToFirebase(notification)
+  }
+
+  /**
+   * reading only the last received message as that's the only important
+   * message here
+   */
+  private fun getFirstMessageFromProvider(context: Context?): String {
+    val cursor: Cursor? =
+      context?.contentResolver?.query(Uri.parse("content://sms/inbox"), null,
+        null, null, null)
+    var message = ""
+    if (cursor?.moveToFirst()!!) {
+      for (i in 0 until cursor.columnCount) {
+        message += " " + cursor.getColumnName(i)
+          .toString() + ":" + cursor.getString(i) + "\n"
       }
     }
+    cursor.close()
+    return message
+  }
+
+  /**
+   * notification : object needs to be added to firebase realtime
+   */
+  private fun addNotificationToFirebase(notification: Notification) {
+    val myFirebaseRef = FirebaseDatabase.getInstance()
+      .getReference(FIREBASE_REFERENCE)
+    myFirebaseRef.child(notification.time)
+      .setValue(notification)
   }
 }
--- app/src/main/java/com/goyals/transfernotification/view/MainActivity.kt
+++ app/src/main/java/com/goyals/transfernotification/view/MainActivity.kt
@@ -1,17 +1,13 @@
 package com.goyals.transfernotification.view
 
 import android.Manifest
-import android.database.Cursor
-import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import com.google.firebase.database.FirebaseDatabase
 import com.goyals.transfernotification.R
 import com.goyals.transfernotification.databinding.ActivityMainBinding
 import com.goyals.transfernotification.model.schema.Notification
-import com.goyals.transfernotification.utils.AppUtils
 import com.permissionx.guolindev.PermissionX
 
 class MainActivity : AppCompatActivity() {
@@ -36,42 +32,14 @@ class MainActivity : AppCompatActivity() {
 
   private fun readMessages() {
     PermissionX.init(this)
-      .permissions(Manifest.permission.READ_SMS)
+      .permissions(Manifest.permission.READ_SMS,
+        Manifest.permission.RECEIVE_SMS)
       .request { allGranted, _, deniedList ->
-        if (allGranted) {
-          startSmsContentProvider()
-        } else {
+        if (!allGranted) {
           Toast.makeText(this, "These permissions are denied: $deniedList",
             Toast.LENGTH_LONG)
             .show()
         }
       }
-  }
-
-  private fun startSmsContentProvider() {
-    val cursor: Cursor? =
-      contentResolver.query(Uri.parse("content://sms/inbox"), null, null, null,
-        null)
-    if (cursor?.moveToFirst()!!) { // must check the result to prevent exception
-      do {
-        var message = ""
-        for (idx in 0 until cursor.columnCount) {
-          message += " " + cursor.getColumnName(idx)
-            .toString() + ":" + cursor.getString(idx) + "\n"
-        }
-        val notification = AppUtils.getNotificationFromMessage(message)
-        addNotificationToFirebase(notification)
-        notificationList.add(notification)
-        notificationAdapter.notifyDataSetChanged()
-      } while (cursor.moveToNext())
-      cursor.close()
-    }
-  }
-
-  private fun addNotificationToFirebase(notification: Notification) {
-    val database = FirebaseDatabase.getInstance()
-    val myRef = database.getReference("notifications")
-    myRef.child(notification.time)
-      .setValue(notification)
   }
 }
